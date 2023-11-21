ef6f1d67e41fd36ba5e88a59612ff3ec1b5a9f8f
Your Name <antoniojz1998@gmail.com>
2023-11-21 16:37:02 +0100

    firebase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:push_app/config/router/app_router.dart';
 import 'package:push_app/config/theme/app_theme.dart';
+import 'package:push_app/presentation/blocs/notifications/notifications_bloc.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await NotificationsBloc.initalizeFirebaseNotifications();
+
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider(create: (_) => NotificationsBloc()),
+    ],
+    child: const MainApp(),
+  ));
 }
 
 class MainApp extends StatelessWidget {
